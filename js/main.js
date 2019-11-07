e66e87a534007545a8223e093a123f82caabd9cb
Siarra Price <siarraprice@ufl.edu>
2019-11-06 20:47:56 -0600

    Update navigation animation

--- js/main.js
+++ js/main.js
@@ -4,16 +4,23 @@ const navSlide = () => {
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
 
-  //toggle nav
   burger.addEventListener('click', () => {
+    //toggle nav
     nav.classList.toggle('nav-active');
+
+    //animate links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = ""
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.6}s`;
+      }
+    });
+
+    //burger animation
+    burger.classList.toggle('toggle');
+
   });
-
-  //animate links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1}s`;
-  })
-
 };
 
 navSlide();
