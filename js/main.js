ce4a231589621291f16d4c3078dc6e589d1123e1
Siarra Price <siarraprice@ufl.edu>
2019-11-08 12:59:22 -0600

    Update navigation background color

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
-//navigation
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-
-  burger.addEventListener('click', () => {
-    //toggle nav
-    nav.classList.toggle('nav-active');
-
-    //animate links
-    navLinks.forEach((link, index) => {
-      if(link.style.animation) {
-        link.style.animation = ""
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-      }
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
     });
-
-    //burger animation
-    burger.classList.toggle('toggle');
-
-  });
-};
-
-navSlide();
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
 
 
 
