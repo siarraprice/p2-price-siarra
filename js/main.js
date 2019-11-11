bc58e0c2c80d9c2877bdbb606ec357d12560ac59
Siarra Price <siarraprice@ufl.edu>
2019-11-10 23:28:09 -0600

    Add FAQ page with accordion functionality

--- js/main.js
+++ js/main.js
@@ -117,3 +117,29 @@ function camp6() {
     moreText.style.display = "inline";
   }
 }
+
+
+
+//accordion functionality
+
+var accordionheaders = document.querySelectorAll("#accordion .accordion-headers");
+var accordionDescriptions = document.querySelectorAll("#accordion .accordion-description");
+
+// handles the accordion dropdown action
+function expandAccordionDescription() {
+
+  // handles the toggle indicator
+  for (i = 0; i < accordionheaders.length; i++) {
+    accordionheaders[i].childNodes[3].innerHTML = '+';
+    accordionDescriptions[i].classList.remove('accordion-open');
+  }
+
+  this.nextElementSibling.classList.toggle('accordion-open');
+  this.childNodes[3].innerHTML = '-';
+
+};
+
+// add click event listener for accordion headers
+for (i = 0; i < accordionheaders.length; i++) {
+  accordionheaders[i].addEventListener("click", expandAccordionDescription);
+};
