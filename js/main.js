77bc9f685e56d03fffa1019937a30b41153d055a
Siarra Price <siarraprice@ufl.edu>
2019-11-11 12:08:07 -0600

    Fix responsive navigation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("nav-dropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("nav-dropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-
-
-
-
-
 
 //read more functionality
 function camp1() {
